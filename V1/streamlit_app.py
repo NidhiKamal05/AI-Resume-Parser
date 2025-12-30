d811e641ef40520b550fef9f6464df1e65601c75
Nidhi Kamal <nidhikamal05@gmail.com>
2025-12-30 20:35:15 +0530

    Update streamlit_app.py

--- V1/streamlit_app.py
+++ V1/streamlit_app.py
@@ -10,9 +10,9 @@ uploaded_file = st.file_uploader("Upload Resume (PDF only)", type=["pdf"])
 if uploaded_file is not None:
     # Assuming resume_parser_v5 is defined in your notebook
     # and expects a file-like object (e.g., in-memory file from Streamlit)
-    parsed_data = resume_parser(uploaded_file)
+    #parsed_data = resume_parser(uploaded_file)
     
     st.subheader("Structured Resume Data")
-    st.json(parsed_data)
+    #st.json(parsed_data)
 else:
     st.info("Please upload a PDF file to get started.")
