2ca09a2205b85a7c1143962acd762bc89216e44d
Nidhi Kamal <nidhikamal05@gmail.com>
2026-01-01 20:04:12 +0530

    Add route for resume parsing
    Added a new route for resume parsing that renders parse.html.

--- V1/AI_Resume_Parser/app.py
+++ V1/AI_Resume_Parser/app.py
@@ -7,5 +7,10 @@ def index():
 	# Renders the index.html file inside the templates folder
 	return render_template('index.html')
 
+@app.route('/analyze')
+def parse_resume():
+	# Renders the index.html file inside the templates folder
+	return render_template('parse.html')
+
 if __name__ == '__main__':
 	app.run(debug = False)  # Ensures debug is False for production
