7b6a74aa3f3fcc80734972e89a5ac32459e0a837
Nidhi Kamal <nidhikamal05@gmail.com>
2026-01-01 19:23:33 +0530

    Fix Flask app initialization and main check

--- V1/AI_Resume_Parser/app.py
+++ V1/AI_Resume_Parser/app.py
@@ -1,11 +1,11 @@
 from flask import Flask, render_template, url_for
 
-app = Flask(_name_)
+app = Flask(__name__)
 
 @app.route('/')
 def index():
 	# Renders the index.html file inside the templates folder
 	return render_template('index.html')
 
-if _name_ == '__main__':
+if __name__ == '__main__':
 	app.run(debug = False)  # Ensures debug is False for production
